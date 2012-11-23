e14e191cf6ae0e61fc36c90e1e07da6110cf43b0
Xiaochao Yang <yxccumt@gmail.com>
2012-11-23 14:23:42 -0800

    added a gitingnore

--- camera_main.c
+++ camera_main.c
@@ -23,65 +23,65 @@ IplImage *color_img, *gray_img;
 
 
 void termination_handler(int signum){
-  printf("\n");
-  free(haar_cascade_path);
-  free(asef_locator_path);
-  cvDestroyWindow(window_title);
-  asef_destroy(&asef);
-  cvReleaseCapture( &capture);
-  cvReleaseImage( &gray_img );
-  cvReleaseImage( &color_img );
-  exit(EXIT_SUCCESS);
+	printf("\n");
+	free(haar_cascade_path);
+	free(asef_locator_path);
+	cvDestroyWindow(window_title);
+	asef_destroy(&asef);
+	cvReleaseCapture( &capture);
+	cvReleaseImage( &gray_img );
+	cvReleaseImage( &color_img );
+	exit(EXIT_SUCCESS);
 }
 
 int main( int argc, char** argv )
 {
 
-  if (parse_arguments(argc, argv, &haar_cascade_path, &asef_locator_path))
-    return -1;
+	if (parse_arguments(argc, argv, &haar_cascade_path, &asef_locator_path))
+		return -1;
 
-  struct sigaction act;
-  act.sa_handler = termination_handler; 
-  sigemptyset(&act.sa_mask);
-  act.sa_flags = 0;
-  sigaction(SIGINT, &act, NULL);
+	struct sigaction act;
+	act.sa_handler = termination_handler; 
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+	sigaction(SIGINT, &act, NULL);
 
-  int frame_width, frame_height;
+	int frame_width, frame_height;
 
-  if ( asef_initialze(&asef, asef_locator_path, haar_cascade_path) )
-    return -1;
+	if ( asef_initialze(&asef, asef_locator_path, haar_cascade_path) )
+		return -1;
 
-  if( !(capture = cvCaptureFromCAM(-1)) ) {
-    fprintf( stderr, "ERROR: capture is NULL \n" );
-    return -1;
-  }
+	if( !(capture = cvCaptureFromCAM(-1)) ) {
+		fprintf( stderr, "ERROR: capture is NULL \n" );
+		return -1;
+	}
 
-  get_camera_properties(capture, &frame_width, &frame_height, NULL);
+	get_camera_properties(capture, &frame_width, &frame_height, NULL);
 
-  CvSize frame_size = cvSize(frame_width, frame_height);
+	CvSize frame_size = cvSize(frame_width, frame_height);
 
-  IplImage* frame = NULL;
-  color_img = cvCreateImage( frame_size, IPL_DEPTH_8U, 3 );
-  gray_img = cvCreateImage( frame_size, IPL_DEPTH_8U, 1);
+	IplImage* frame = NULL;
+	color_img = cvCreateImage( frame_size, IPL_DEPTH_8U, 3 );
+	gray_img = cvCreateImage( frame_size, IPL_DEPTH_8U, 1);
 
-  cvNamedWindow(window_title, 0);
-  while (1){
-    printf("\r"); fflush(stdout); 
-    printf("FPS: %f  ", calculate_fps());
+	cvNamedWindow(window_title, 0);
+	while (1){
+		printf("\r"); fflush(stdout); 
+		printf("FPS: %f  ", calculate_fps());
 
-    frame = cvQueryFrame(capture);
-    cvCopyImage(frame, color_img);
-    cvCvtColor(color_img, gray_img, CV_RGB2GRAY);
-    asef.input_image = gray_img;
+		frame = cvQueryFrame(capture);
+		cvCopyImage(frame, color_img);
+		cvCvtColor(color_img, gray_img, CV_RGB2GRAY);
+		asef.input_image = gray_img;
 
-    if( asef_detect_face(&asef) ){
-      asef_locate_eyes(&asef);
-      draw_markers(color_img, asef.face_rect, asef.left_eye, asef.right_eye);
-    }
+		if( asef_detect_face(&asef) ){
+			asef_locate_eyes(&asef);
+			draw_markers(color_img, asef.face_rect, asef.left_eye, asef.right_eye);
+		}
 
     // Mirroring results. assuming you're using a frontal facing camera
-    cvFlip(color_img, color_img, 1);
-    cvShowImage( window_title, color_img);
-  } 
-  return 0;
+		cvFlip(color_img, color_img, 1);
+		cvShowImage( window_title, color_img);
+	} 
+	return 0;
 }
