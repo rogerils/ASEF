23e47a3f2c35e793f97172b4b06581a538c05f7a
Xiaochao Yang <yxccumt@gmail.com>
2012-11-23 14:33:08 -0800

    fixing consistent indentation

--- fd.c
+++ fd.c
@@ -2,28 +2,28 @@
 
 CvHaarClassifierCascade* fd_load_detector( const char* cascade_path )
 {
-  return (CvHaarClassifierCascade*)cvLoad( cascade_path, NULL, NULL, NULL);
+	return (CvHaarClassifierCascade*)cvLoad( cascade_path, NULL, NULL, NULL);
 }
 
 int fd_detect_face(IplImage* image, CvHaarClassifierCascade* cascade, CvRect *rect, CvMemStorage* buffer){
-  
-  CvSeq* faces;
-  int rv = 0;
+	
+	CvSeq* faces;
+	int rv = 0;
 
 /* use the fastest variant */
-  faces = cvHaarDetectObjects( image, cascade, buffer, 1.2, 3, 
-    CV_HAAR_DO_CANNY_PRUNING | CV_HAAR_FIND_BIGGEST_OBJECT | CV_HAAR_DO_ROUGH_SEARCH, 
-    cvSize(0, 0), cvSize(0, 0));
+	faces = cvHaarDetectObjects( image, cascade, buffer, 1.2, 3, 
+		CV_HAAR_DO_CANNY_PRUNING | CV_HAAR_FIND_BIGGEST_OBJECT | CV_HAAR_DO_ROUGH_SEARCH, 
+		cvSize(0, 0), cvSize(0, 0));
 
 
 /* draw all the rectangles */
-  if ( faces->total > 0)
-  {
+	if ( faces->total > 0)
+	{
 /* extract the rectanlges only */
-    *rect = *(CvRect*)cvGetSeqElem( faces, 0 );
-    rv = 1;
-  } else {
-    rv = 0;
-  }
-  return rv;
+		*rect = *(CvRect*)cvGetSeqElem( faces, 0 );
+		rv = 1;
+	} else {
+		rv = 0;
+	}
+	return rv;
 }
